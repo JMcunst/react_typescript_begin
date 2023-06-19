e589422b0c29bc315c253ade68bdd2b3dbc60d3d
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-19 21:24:10 +0900

    init: title

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,10 @@
 import React from 'react';
 import './App.css';
 
-// // Why we need TypeScript?
-// let name: string;
-// let age: number;
-// let isStudent: boolean;
-// let hobbies: string[];
-// let role: [number, string];
-
-// type Person = {
-//   name: string;
-//   age: number;
-// };
-// let person: Person = {
-//   name: "JMcunst",
-//   age: 28
-// };
-// type Person2 = {
-//   name: string;
-//   age?: number;
-// };
-// let person2: Person2 = {
-//   name: "Jmcunst"
-// }
-// let lotsOfPeople: Person[];
-
-// let score: number | string;
-// function printScore(score: number){
-//   console.log(score);
-// }
-// let printScore2: Function;
-// let printScore3: (score: number) => void;
-// let printScore4: (score: number) => never;
-// let personName: unknown;
-
-// interface Person {
-//   name: string;
-//   age?: number;
-// }
-
-// type X = {
-//   a: string;
-//   b: number;
-// }
-// type Y = X & { // need remove X &
-//   c: string;
-//   d: number;
-// }
-// let y:Y = {
-//   c: 'test',
-//   d: 33
-// }
-// //
-// interface Guy extends Person{
-//   profession: string;
-// }
-// //
-// type W = Person & {
-//   a: string;
-//   b: number;
-// }
-// //
-// interface Person extends X {
-//   name: string;
-//   age?: number;
-// }
-
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
-    Hello World
+      <span className='heading'>Jmcunst</span>
     </div>
   );
 }
