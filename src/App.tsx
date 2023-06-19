5bdef68df5715df7c42f7af31c331e350e1f85a2
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-19 21:08:36 +0900

    test: typescript variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import './App.css';
 
+// // Why we need TypeScript?
+// let name: string;
+// let age: number;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
+
+// type Person = {
+//   name: string;
+//   age: number;
+// };
+// let person: Person = {
+//   name: "JMcunst",
+//   age: 28
+// };
+// type Person2 = {
+//   name: string;
+//   age?: number;
+// };
+// let person2: Person2 = {
+//   name: "Jmcunst"
+// }
+// let lotsOfPeople: Person[];
+
+// let score: number | string;
+// function printScore(score: number){
+//   console.log(score);
+// }
+// let printScore2: Function;
+// let printScore3: (score: number) => void;
+// let printScore4: (score: number) => never;
+// let personName: unknown;
+
 function App() {
   return (
     <div className="App">
