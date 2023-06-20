93860bd50ac6d1ff98471496811ddefec3d87714
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-20 09:17:09 +0900

    update: handle add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
 const App:React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo:todo, isDone: false}])
+      setTodo("");
+    }
+  };
 
   console.log('test:',todo);
 
   return (
     <div className="App">
       <span className='heading'>Jmcunst</span>
-      <InputFeild todo={todo} setTodo={setTodo}/>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
